36e42fd8e8f84a76b5844773cd45178d46c10401
Sebastien Luong <sebastienluong@gmail.com>
2016-06-22 07:27:51 -0700

    Add stroke style

--- styleColors.js
+++ styleColors.js
@@ -5,8 +5,11 @@ function draw() {
   if (canvas.getContext){
     var ctx = canvas.getContext('2d');
 
+    // strokeStyle (borders)
+    drawStrokeStyle(ctx);
     // fillStyle eg
-    drawFillStyle(ctx);
+    //drawFillStyle(ctx);
+
   }
 }
 
@@ -19,41 +22,18 @@ function drawFillStyle(ctx){
   }
 }
 
-function draw2dCircle(ctx){
-  var circle = new Path2D();
-  circle.moveTo(125,35);
-  circle.arc(100,35,25,0,2*Math.PI);
-  ctx.fill(circle);
+function drawStrokeStyle(ctx){
+  for (var i=0; i<6; i++ ){
+    for (var j=0; j<6; j++){
+      ctx.strokeStyle = 'rgb(0,'+Math.floor(255-42.5*i) +','+
+                        Math.floor(255-42.5*j)+')';
+      ctx.beginPath();
+      ctx.arc(12.5+j*25, 12.5+i*25, 10, 0, Math.PI*2, true);
+      ctx.stroke();
+    }
+  }
 }
 
-function drawSmiley(ctx){
-  ctx.beginPath();
-  ctx.arc(75,75,50,0, Math.PI*2, true);
-  ctx.moveTo(110,75);
-  ctx.arc(75,75,35, 0, Math.PI, false);
-  ctx.moveTo(65,65);
-  ctx.arc(60,65,5,0,Math.PI*2, true);
-  ctx.moveTo(95,65);
-  ctx.arc(90,65,5,0,Math.PI*2, true);
-  ctx.stroke();
-}
-
-function drawTriangle(ctx){
-    ctx.beginPath();
-    ctx.moveTo(75,50);
-    ctx.lineTo(100, 75);
-    ctx.lineTo(100, 25);
-    ctx.fill();
-}
-
-function drawOverlappedRect(ctx){
-    ctx.fillStyle = "rgb(200,0,0)";
-    ctx.fillRect(10,10,50,50);
-
-    ctx.fillStyle = "rgba(0,0,200,0.5)";
-    ctx.fillRect(30,30,50,50);
-
-}
 
 window.onload = function() {
   draw();
