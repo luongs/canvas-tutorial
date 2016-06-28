6b94762f13603a19610c40aec0f1c843fc842659
Sebastien Luong <sebastienluong@gmail.com>
2016-06-27 22:32:36 -0700

    Add transparency and selection rectangle

--- styleColors.js
+++ styleColors.js
@@ -5,8 +5,9 @@ function draw() {
   if (canvas.getContext){
     var ctx = canvas.getContext('2d');
 
+    drawRgba(ctx);
     // strokeStyle (borders)
-    drawStrokeStyle(ctx);
+    //drawStrokeStyle(ctx);
     // fillStyle eg
     //drawFillStyle(ctx);
 
@@ -22,6 +23,7 @@ function drawFillStyle(ctx){
   }
 }
 
+// Draw border
 function drawStrokeStyle(ctx){
   for (var i=0; i<6; i++ ){
     for (var j=0; j<6; j++){
@@ -34,6 +36,32 @@ function drawStrokeStyle(ctx){
   }
 }
 
+
+// Draw semi-transparent shapes
+function drawRgba(ctx){
+  // Draw background
+  ctx.fillStyle = 'rgb(255,221,0)';
+  ctx.fillRect(0,0,150,37.5);
+  ctx.fillStyle = 'rgb(102,204,0)';
+  ctx.fillRect(0,37.5,150,37.5);
+  ctx.fillStyle = 'rgb(0,153,255)';
+  ctx.fillRect(0,75,150,37.5);
+  ctx.fillStyle = 'rgb(255,51,0)';
+  ctx.fillRect(0,112.5,150,37.5);
+
+  // Draw semi transparent rectangles
+  for (var i=0; i<10; i++){
+    ctx.fillStyle = 'rgba(255, 255, 255,'+(i+1)/10+')';
+      for (var j=0; j<4; j++){
+        ctx.fillRect(5+i*14, 5+j*37.5, 14, 27.5);
+      }
+  }
+}
+
+// Draw gradient
+function drawLinearGradient(ctx){
+
+}
 
 window.onload = function() {
   draw();
