cfc5b70208a4d9482d8c60b8eb97dd51e8bba921
Sebastien Luong <sebastienluong@gmail.com>
2016-07-04 15:50:43 -0700

    Add boundary detection

--- animateAdvanced.js
+++ animateAdvanced.js
@@ -1,16 +1,19 @@
 "use strict"
 
+let raf;  // RequestAnimationFrame
 function draw(){
   let canvas = document.getElementById('tutorial');
   if (canvas.getContext){
     let ctx = canvas.getContext('2d');
-    ball.draw(ctx);
+    drawMovingBall(ctx, canvas);
   }
 }
 
 let ball = {
   x: 100,
   y: 100,
+  vx: 5,
+  vy: 2,
   radius: 25,
   color: 'blue',
   draw: function(ctx){
@@ -22,7 +25,23 @@ let ball = {
   }
 };
 
+function drawMovingBall(ctx, canvas){
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ball.draw(ctx);
+  ball.x += ball.vx;
+  ball.y += ball.vy;
+
+  // Add boundary detection
+  if (ball.x + ball.vx > canvas.width || ball.x + ball.vx < 0){
+    ball.vx = -ball.vx;
+  }
+  if (ball.y + ball.vy > canvas.height || ball.y + ball.vy < 0){
+    ball.vy = -ball.vy;
+  }
+  raf = window.requestAnimationFrame(draw);
+}
+
 
 window.onload = function() {
-  draw();
+  raf = window.requestAnimationFrame(draw);
 }
